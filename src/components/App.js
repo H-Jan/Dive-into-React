92a09eeb4eed825891a53f428612ffe6c26dff40
Hani J <hanijandali@gmail.com>
2021-12-06 23:39:04 -0800

    Stylistic Elements added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 // import React from 'react';
 import logo from './logo.svg';
-import POPOSList from './POPOSList';
+import POPOSList from './POPOSList/POPOSList';
 import { Outlet } from 'react-router-dom'
 import './App.css';
-import Title from './Title';
+import Title from './Title/Title';
 
 
 function App() {
--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -1,19 +1,25 @@
-import logo from './logo.svg'
+import logo from '../logo.svg'
 import { Link } from 'react-router-dom'
 
 function POPOSSpace(props) {
     const {name, image, address, hours, id } = props 
     return (
-        <div> 
-            <Link to={`/details/${id}`}>
+        <div className="POPOSSpace"> 
+            <h1>
+            <Link 
+                className="POPOSSpace-title"
+                to={`/details/${id}`}>
                 {name}
             </Link>
+            </h1>
             <Link to={`/details/${id}`}>
                 <img src={`${process.env.PUBLIC_URL}images/${image}`} 
                 width="300" height="300" alt="Hello" />
             </Link>
-            <div>{address}</div>
-            <div>{hours}</div>
+            <div className="POPOSSpace-info">
+                <div>{address}</div>
+                <div>{hours}</div>
+            </div>
         </div>
     )
 }
